9a0946eedec369380d9f49b7c0c391aa188b6f64
Jyotheesh Chekuri <jyotheesh1999@gmail.com>
2021-06-25 18:35:40 +0530

    added projects order

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,13 @@
+#portfoliomodels
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
 class Project(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=200)
+    created = models.DateTimeField(default=datetime.now())
     image=models.ImageField(upload_to="portfolio/images/")
     url=models.URLField(blank=True)
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,8 @@
+#portfolioviews
 from django.shortcuts import render
 from .models import Project
 # Create your views here.
 def home(request):
-    projects=Project.objects.all()
+    projects=Project.objects.all().order_by('-created')
     return render(request,'portfolio/home.html',{'projects':projects})
 
